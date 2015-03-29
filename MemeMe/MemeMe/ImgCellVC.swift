f2cdd8931195903f8d1c94659513d415b1a13b6d
jeffreyhzhang <jeffreyhzhang@users.noreply.github.com>
2015-03-29 09:24:02 -0400

    Committed by Udacity

---
+++ MemeMe/MemeMe/ImgCellVC.swift
@@ -0,0 +1,13 @@
+//
+//  ImgCellVC.swift
+//  MemeMe
+//
+//  Created by JeffreyLee on 3/19/15.
+//  Copyright (c) 2015 Jeffrey. All rights reserved.
+//
+
+import UIKit
+
+class ImgCellVC: UICollectionViewCell {
+    @IBOutlet weak var imgvw: UIImageView!
+}
